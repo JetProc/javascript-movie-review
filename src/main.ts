bd0bff7926c3b4504c0cc6a97ece986431bf1b07
JetProc <soonjae8297@gmail.com>
2026-03-31 16:56:58 +0900

    feat: popular API 호출 기능 구현

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,18 @@ addEventListener("load", () => {
 
   if (app) {
     app.appendChild(buttonImage);
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+      },
+    };
+
+    fetch("https://api.themoviedb.org/3/movie/popular?page=1", options)
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
   }
 });
