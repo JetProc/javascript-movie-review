a8e0e6b560d7db23c9ad4db3c47d2e1953a51b8a
JetProc <soonjae8297@gmail.com>
2026-04-02 09:25:03 +0900

    fix: default-thumbnail-image가 제대로 나오지 않던 버그 수정
    Co-authored-by: Eastar-DS <Eastar-DS@users.noreply.github.com>

--- src/utils/MovieUtil.ts
+++ src/utils/MovieUtil.ts
@@ -1,2 +1,4 @@
-export const createImageUrl = (baseUrl: string, imageUrlPath: string) =>
-  imageUrlPath ? `${baseUrl}${imageUrlPath}` : `../images/default-thumbnail.jpeg`;
+export const createImageUrl = (baseUrl: string, imageUrlPath: string) => {
+  console.log(imageUrlPath.length > 0);
+  return imageUrlPath.length > 0 ? `${baseUrl}${imageUrlPath}` : `/images/default-thumbnail.jpeg`;
+};
