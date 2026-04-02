1e0b9f598c13704e94e82c90ceb69ae9de1c6972
JetProc <soonjae8297@gmail.com>
2026-04-02 11:17:39 +0900

    perf: 포스터 썸네일 크기 축소

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = {
-  POSTER_BASE_URL: "https://image.tmdb.org/t/p/w500",
+  POSTER_BASE_URL: "https://image.tmdb.org/t/p/w200",
   HERO_BASE_URL: "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces",
 };
 
