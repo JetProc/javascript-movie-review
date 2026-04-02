dd7c3eac5b70833b35eba33fe9cc08e64ee311f2
JetProc <soonjae8297@gmail.com>
2026-04-02 11:17:44 +0900

    chore: 이미지 URL 디버그 로그 제거

--- src/utils/MovieUtil.ts
+++ src/utils/MovieUtil.ts
@@ -1,6 +1,5 @@
 import { IMAGE_URL } from "../constants/constant";
 
 export const createImageUrl = (baseUrl: string, imageUrlPath: string) => {
-  console.log(imageUrlPath.length > 0);
   return imageUrlPath.length > 0 ? `${baseUrl}${imageUrlPath}` : IMAGE_URL.DEFAULT_THUMBNAIL_IMAGE_URL;
 };
