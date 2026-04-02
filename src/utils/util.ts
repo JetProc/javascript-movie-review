8bb7eacbf5273adee9b5c6e51629d5e51d978634
JetProc <soonjae8297@gmail.com>
2026-04-02 16:32:08 +0900

    Updates

--- src/utils/util.ts
+++
@@ -1,9 +0,0 @@
-export const $ = <T extends Element>(selector: string): T => {
-  const element = document.querySelector(selector);
-
-  if (!element) {
-    throw new Error(`${selector} 요소를 찾을 수 없습니다`);
-  }
-
-  return element as T;
-};
--- types/movie.ts
+++
@@ -1,7 +0,0 @@
-export interface Movie {
-  id: number;
-  title: string;
-  rate: number;
-  thumbnail_path: string | null;
-  hero_path: string | null;
-}
