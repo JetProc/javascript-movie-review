4a34ac9674c02d10dc9635ed84d4fc4569b7702d
JetProc <soonjae8297@gmail.com>
2026-04-02 09:29:20 +0900

    fix: thumbnail-path 이미지 경로 로드 로직 수정

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { AppElements } from "../types/dom";
 import { Movie } from "../types/movie";
 import { fetchMoviePageData } from "./API/api";
 import { FetchMoviePageDataResponse } from "./API/config";
-import { BASE_URL, SKELETON_MOVIE_COUNT } from "./constants/constant";
-import starImage from "./images/star_empty.png";
+import { BASE_URL, SKELETON_MOVIE_COUNT, STAR_IMAGE_URL } from "./constants/constant";
 import { State } from "../types/state";
 import { createImageUrl } from "./utils/MovieUtil";
 import { $ } from "./utils/util";
@@ -34,28 +33,19 @@ const getAppElements = (): AppElements => ({
 const fetchMoviePages = async (query: string): Promise<FetchMoviePageDataResponse> => {
   const response: FetchMoviePageDataResponse = await fetchMoviePageData(state.currentPage + 1, query);
 
-  updateMovieState({
-    currentPage: response.currentPage,
-    totalPage: response.totalPages,
-    movieList: response.results,
-    query: query,
-  });
-
   return response;
 };
 
 //
 const createMovieListItemMarkup = (movie: Movie) => {
-  if (!movie.thumbnail_path) movie.thumbnail_path = `../images/default-thumbnail.jpeg`;
-
-  const posterImageUrl = createImageUrl(BASE_URL.POSTER_BASE_URL, movie.thumbnail_path);
+  const posterImageUrl = createImageUrl(BASE_URL.POSTER_BASE_URL, movie.thumbnail_path ?? "");
 
   return /* html */ `<li>
     <div class="item">
       <img class="thumbnail" src="${posterImageUrl}" alt="${movie.title}" />
       <div class="item-desc">
         <p class="rate">
-          <img src="${starImage}" class="star" alt="" aria-hidden="true" />
+          <img src="${STAR_IMAGE_URL}" class="star" alt="" aria-hidden="true" />
           <span>${movie.rate}</span>
         </p>
         <strong>${movie.title}</strong>
@@ -69,7 +59,7 @@ const renderMovies = (movies: Movie[], movieListElement: HTMLUListElement) => {
 };
 
 const renderHeroMovie = (movie: Movie, elements: AppElements) => {
-  const posterImageUrl = createImageUrl(BASE_URL.HERO_BASE_URL, movie.hero_path!);
+  const posterImageUrl = createImageUrl(BASE_URL.HERO_BASE_URL, movie.hero_path ?? "");
 
   elements.heroBackdrop.style.backgroundImage = posterImageUrl ? `url("${posterImageUrl}")` : "";
   elements.heroRate.hidden = false;
@@ -84,55 +74,7 @@ const updateMovieState = (newState: State) => {
   state.movieList = newState.movieList;
 };
 
-// const createMovieQuery = (query: string) => {
-//   const normalizedQuery = query.trim();
-
-//   return normalizedQuery;
-// };
-
-const loadMovies = async (state: State, elements: AppElements, query = "") => {
-  elements.skeletonCard.innerHTML = makeSkeleton();
-
-  // const movieQuery = createMovieQuery(query);
-  const firstPage = await fetchMoviePages(query);
-
-  //popular일 때만 hero => state에 query가 없을때만 hero
-  // if (!state.query) {
-  //   renderHeroMovie(firstPage.results[0], elements.hero);
-  // }
-  if (!state.query) {
-    renderHeroMovie(firstPage.results[0], elements);
-  }
-
-  elements.skeletonCard.innerHTML = "";
-
-  renderMovies(state.movieList, elements.movieList);
-  // console.log(state.visibleMovies);
-};
-
-const initializeMoviePage = async (state: State, elements: AppElements) => {
-  await loadMovies(state, elements);
-
-  elements.searchForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    await loadMovies(state, elements, elements.searchInput.value);
-  });
-};
-
-const main = async () => {
-  const elements = getAppElements();
-
-  // bindEvents(elements);
-
-  await initializeMoviePage(state, elements);
-};
-
-//todo: throw error를 콘솔이 아니라 ui로 변경해야 함
-window.addEventListener("load", () => {
-  void main().catch((error) => console.log(error));
-});
-
-const makeSkeleton = () => {
+const makeSkeleton = (skeletonCardElement: HTMLUListElement) => {
   const skeletonItemMarkup = /* html */ `<li>
     <div class="item" aria-hidden="true">
       <div class="thumbnail thumbnail-skeleton skeleton"></div>
@@ -146,5 +88,59 @@ const makeSkeleton = () => {
     </div>
   </li>`;
 
-  return Array.from({ length: SKELETON_MOVIE_COUNT }, () => skeletonItemMarkup).join("");
+  skeletonCardElement.innerHTML = Array.from({ length: SKELETON_MOVIE_COUNT }, () => skeletonItemMarkup).join("");
+};
+
+const loadMovies = async (elements: AppElements) => {
+  makeSkeleton(elements.skeletonCard);
+
+  const response = await fetchMoviePages(state.query);
+
+  updateMovieState({
+    currentPage: response.currentPage,
+    totalPage: response.totalPages,
+    movieList: [...state.movieList, ...response.results],
+    query: state.query,
+  });
+
+  elements.skeletonCard.innerHTML = "";
+
+  renderMovies(state.movieList, elements.movieList);
+};
+
+const initializeMoviePage = async (elements: AppElements) => {
+  await loadMovies(elements);
+  renderHeroMovie(state.movieList[0], elements);
+};
+
+const main = async () => {
+  const elements = getAppElements();
+
+  bindEvents(elements);
+
+  await initializeMoviePage(elements);
+};
+
+//todo: throw error를 콘솔이 아니라 ui로 변경해야 함
+window.addEventListener("load", () => {
+  void main().catch((error) => console.log(error));
+});
+
+const bindEvents = (elements: AppElements) => {
+  elements.seeMoreBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    //1. API 호출
+    await loadMovies(elements);
+
+    //3. 그리기
+  });
+
+  elements.searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    //state의 쿼리를 elements.searchInput.value로 업데이트
+
+    await loadMovies(elements);
+  });
 };
